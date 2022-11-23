ca40ae7edb7cac6e43ef10cfafc4fe74fbf2dd04
David Windler <david.windler@valtech.com>
2022-11-23 15:51:54 +0100

    change folder structure for threejs demos

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+import glob from "glob";
+
+export default defineConfig({
+  // plugins: [basicSsl()],
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: glob.sync(resolve(__dirname, "src", "*.html")),
+    },
+  },
+  // server: {
+  //   https: true,
+  //   host: "0.0.0.0",
+  // },
+});
